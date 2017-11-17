636799c9512c16b01a3dca4d2bbfd04f30ba945b
MarcosHernandezRodriguez <marcoshdezrguez@hotmail.com>
2017-11-17 14:09:14 +0000

    Kata-5P1 Version-2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -5,6 +5,11 @@
  */
 package kata5p1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,7 +27,7 @@ public class Kata5P1 {
      * @throws java.lang.ClassNotFoundException
      * @throws java.sql.SQLException
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         // TODO code application logic here
         
         Class.forName("org.sqlite.JDBC");	
@@ -33,11 +38,43 @@ public class Kata5P1 {
         String query = "SELECT * FROM PEOPLE";
         ResultSet rs = st.executeQuery(query);
         
+        String query2 = "CREATE TABLE IF NOT EXISTS MAIL('Id' INTEGER PRIMARY KEY AUTOINCREMENT , 'Mail' TEXT NOT NULL);";      
+        st.execute(query2);
+        
+        
+        
+        String filename = "C:/Marcos/Universidad/Tercer Año/IS2/emails.txt";    
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+            String mail;
+            
+            while((mail = reader.readLine()) != null){
+                if(!mail.contains("@")){
+                    continue;
+                }
+                query ="INSERT INTO MAIL (mail) VALUES ('" + mail + "')";
+                st.executeUpdate(query);
+                
+            }
+        } catch(FileNotFoundException ex){}
+        
+        
+        
+        
+        String mail = "";
+        String query3 = "insert into mail (Mail) VALUES ('" + mail + "');";      
+        st.execute(query3);
+        
+        String query4 = "SELECT COUNT (*) FROM MAIL";      
+        ResultSet rs4 = st.executeQuery(query4);
+        
         
         while(rs.next()){
             System.out.println(rs.getInt(1));
-            System.out.println(rs.getString(2));
         }
+        
+        
+        
     }
     
 }
